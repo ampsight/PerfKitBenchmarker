5b532bd134155fa63304797a60690e2f9b0ba7c1
sam.parsa <sam.parsa@ampsight.com>
2024-07-11 11:53:52 -0400

    Wrote assumptions of file placements in intern_bash.sh

--- bash_scripts/intern_bash.sh
+++ bash_scripts/intern_bash.sh
@@ -5,4 +5,8 @@ do
     perfkitbenchmarker/pkb.py --benchmarks=coremark --cloud=AWS --benchmark_config_file=intern_testing_config.yaml --ip_addresses=EXTERNAL
 done
 mkdir /tmp/perkitbenchmarker/output
+#Line below assumes that the python script must be in the correct directory
+#tmp -> perfkitbenchmarker -> output & runs & pkb_script_final.py
+#output folder contains the csv's of data and calculations results for each benchmark
+#runs folder contains all the runs and results json's
 python /tmp/perfkitbenchmarker/pkb_script_final.py
